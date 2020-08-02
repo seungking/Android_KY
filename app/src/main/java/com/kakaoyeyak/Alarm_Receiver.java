031b6d129c50202cc91221cbbed850af14092a15
Seungki An <55059074+seungking@users.noreply.github.com>
2020-08-02 16:11:25 +0900

    20200802
    - default 프로필 사진 설정
    - 저장된 인텍스 찾는 방식 변경
    - 서비스 실행시 자동삭제 기능

--- app/src/main/java/com/kakaoyeyak/Alarm_Receiver.java
+++ app/src/main/java/com/kakaoyeyak/Alarm_Receiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 
 public class Alarm_Receiver extends BroadcastReceiver {
     @Override
@@ -11,6 +12,7 @@ public class Alarm_Receiver extends BroadcastReceiver {
         Intent sIntent = new Intent(context, Send_Msg.class);
         sIntent.putExtra("state", intent.getStringExtra("state"));
 
+        Log.d("log1","Alarm_Receiver");
         // Oreo(26) 버전 이후부터는 Background 에서 실행을 금지하기 때문에 Foreground 에서 실행해야 함
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             context.startForegroundService(sIntent);
