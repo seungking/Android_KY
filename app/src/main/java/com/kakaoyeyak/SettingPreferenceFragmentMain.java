b26f47aa2bbf301dc109331c9aa26a9c0eb7a889
namgonkim <32921225+namgonkim@users.noreply.github.com>
2020-08-07 17:22:18 +0900

    t_v1
    t_v1

--- app/src/main/java/com/kakaoyeyak/SettingPreferenceFragmentMain.java
+++ app/src/main/java/com/kakaoyeyak/SettingPreferenceFragmentMain.java
@@ -19,41 +19,15 @@ public class SettingPreferenceFragmentMain extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle bundle, String s) {
         setPreferencesFromResource(R.xml.setting_main, s);
 
-        if (s == null) {
-            soundPreference = findPreference("sound_list");
-//            keywordSoundPreference = findPreference("keyword_sound_list");
-            keywordScreen = findPreference("keyword_screen");
-
-            prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-
-            if (!prefs.getString("sound_list", "").equals("")) {
-                soundPreference.setSummary(prefs.getString("sound_list", "카톡"));
-            }
-
-            if (prefs.getBoolean("keyword", false)) {
-                keywordScreen.setSummary("사용");
-            }else {
-                keywordScreen.setSummary("사용안함");
-            }
-
-            prefs.registerOnSharedPreferenceChangeListener(prefListener);
-        }
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        prefs.registerOnSharedPreferenceChangeListener(prefListener);
 
     }
 
     SharedPreferences.OnSharedPreferenceChangeListener prefListener = new SharedPreferences.OnSharedPreferenceChangeListener() {
         @Override
         public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-            if (key.equals("sound_list")) {
-                soundPreference.setSummary(prefs.getString("sound_list", "카톡"));
-            }
-            if (key.equals("keyword")) {
-                if (prefs.getBoolean("keyword", false)) {
-                    keywordScreen.setSummary("사용");
-                }else {
-                    keywordScreen.setSummary("사용안함");
-                }
-            }
+
 
         }
     };
--- app/src/main/java/com/kakaoyeyak/YeyakMain.java
+++ app/src/main/java/com/kakaoyeyak/YeyakMain.java
@@ -203,6 +203,8 @@ public class YeyakMain extends AppCompatActivity {
                     break;
 
                 case R.id.btn_clear:
+
+                    //clearing();
                     // 데이터 쌓이면 클리어.
                     B_id.clear();
                     Time.clear();
@@ -229,119 +231,3 @@ public class YeyakMain extends AppCompatActivity {
         }
     };
 }
-
-/*
-//예약 메시지 전송 액티비티
-public class YeyakMain extends AppCompatActivity {
-
-    AlarmManager alarm_manager;
-    TimePicker alarm_timepicker;
-    Context context;
-    PendingIntent pendingIntent;
-    EditText katok_msg; // 에딧 텍스트 창에 메시지 입력
-
-    ArrayList<String> Hour = new ArrayList<String>();
-    ArrayList<String> Minute = new ArrayList<String>();
-    ArrayList<String> Id = new ArrayList<String>(); // User UID
-    ArrayList<String> Name = new ArrayList<String>();
-    ArrayList<String> Message = new ArrayList<String>();
-
-    ManagePref managePref = new ManagePref();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_yeyak_main);
-
-        // 전 페이지에서 선택한 유저들 정보 intent에 담아 보낸걸 여기서 받음.
-        // 어뎁터프렌드-putExtra -> 예약메인-getExtra
-        Intent intent = getIntent(); // 데이터 수신
-        final String UID = intent.getExtras().getString("UserUID");
-        final String ID = intent.getExtras().getString("UserID");
-        final String NName = intent.getExtras().getString("UserName");
-
-        // 로그로 선택된 사람 확인해보라고 남겨놨음.
-        // I/보낼 사람 UID,ID,Name:: 4dPm0ObT49ThzfrD8MDzy_rK5tLn1uXS4JI/1395494438/안승기
-        Log.i("보낼 사람 UID,ID,Name: ",UID+"/"+ID+"/"+NName);
-
-        this.context = this;
-
-        //시작 시 정보 받아옴
-        //저장된 배열에 추가로 저장을 하고 다시 저장할거여서
-        Hour = managePref.getStringArrayPref(this,"hour");
-        Minute = managePref.getStringArrayPref(this,"minute");
-        Id = managePref.getStringArrayPref(this,"id");
-        Name = managePref.getStringArrayPref(this,"name");
-        Message = managePref.getStringArrayPref(this,"message");
-
-        // 자꾸 데이터 쌓여서 클리어시킴 여기서
-
-        Hour.clear();
-        Minute.clear();
-        Id.clear();
-        Name.clear();
-        Message.clear();
-
-
-
-        // 현재까지 저장된 넘들 확인해봄.
-        Log.e("Hour",Hour.toString());
-        Log.e("Minute",Minute.toString());
-        Log.e("UUID",Id.toString());
-        Log.e("Name",Name.toString());
-        Log.e("Msg",Message.toString());
-
-        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarm_timepicker = findViewById(R.id.time_picker);
-
-        final Calendar calendar = Calendar.getInstance();
-        final Intent my_intent = new Intent(this.context, Alarm_Receiver.class);
-        katok_msg = (EditText) findViewById(R.id.katok_msg);
-
-        // 알람 시작 버튼
-        Button alarm_on = findViewById(R.id.btn_start);
-        alarm_on.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-
-                // calendar에 시간 셋팅
-                calendar.set(Calendar.HOUR_OF_DAY, alarm_timepicker.getHour());
-                calendar.set(Calendar.MINUTE, alarm_timepicker.getMinute());
-
-                // 시간 가져옴
-                int hour = alarm_timepicker.getHour();
-                int minute = alarm_timepicker.getMinute();
-                Toast.makeText(YeyakMain.this,"Alarm 예정 " + hour + "시 " + minute + "분",Toast.LENGTH_SHORT).show();
-
-                //사용자에게 값 받은
-                //따로 레이아웃 추가하고 값 설정 필요
-                Hour.add(String.valueOf(hour));
-                Minute.add(String.valueOf(minute));
-                Id.add(UID); // 선택한 유저 UID 저장함
-                Name.add(NName); // 선택한 유저 닉네임 저장함
-                Message.add(katok_msg.getText().toString()); // 보낼 메세지 저장함
-
-                //로컬에 업데이트
-                managePref.setStringArrayPref(YeyakMain.this,"hour",Hour);
-                managePref.setStringArrayPref(YeyakMain.this,"minute",Minute);
-                managePref.setStringArrayPref(YeyakMain.this,"id",Id);
-                managePref.setStringArrayPref(YeyakMain.this,"name",Name);
-                managePref.setStringArrayPref(YeyakMain.this,"message",Message);
-
-
-                //시작 및 서비스 등록
-                pendingIntent = PendingIntent.getBroadcast(YeyakMain.this, 0, my_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-                alarm_manager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-
-                startActivity(new Intent(YeyakMain.this,HorizontalNtbActivity.class));
-                //finish();
-
-
-            }
-        });
-    }
-}
-
- */
